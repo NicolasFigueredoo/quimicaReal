2eb9dfb541e4a2915b159d4cd0f7d7e69b419efa
unknown <gtyt@outlook.com.ar>
2024-08-26 15:06:52 -0300

    cambios quimica

---
+++ node_modules/vue-recaptcha-v3/dist/IReCaptchaOptions.d.ts
@@ -0,0 +1,5 @@
+import { IReCaptchaLoaderOptions } from 'recaptcha-v3/dist/ReCaptchaLoader';
+export interface IReCaptchaOptions {
+    siteKey: string;
+    loaderOptions: IReCaptchaLoaderOptions;
+}
